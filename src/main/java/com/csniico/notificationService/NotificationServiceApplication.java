6b1e20304e320fb50ed5412dd1712c8263a95fb3
Christian Solomon <cncs.chris@gmail.com>
2025-05-15 15:47:08 +0000

    add exporters for jaeger and prometheus

--- src/main/java/com/csniico/notificationService/NotificationServiceApplication.java
+++ src/main/java/com/csniico/notificationService/NotificationServiceApplication.java
@@ -2,12 +2,20 @@ package com.csniico.notificationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+		System.out.println(System.getenv("JAEGER_TRACING_URL"));
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
