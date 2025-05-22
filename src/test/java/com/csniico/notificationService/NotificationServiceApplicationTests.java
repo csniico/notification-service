9974420c2dd30ca1f2f70fb9de1a0ad6e3a334e2
Christian Solomon <cncs.chris@gmail.com>
2025-05-22 11:30:02 +0000

    rebase history

--- src/test/java/com/csniico/notificationService/NotificationServiceApplicationTests.java
+++ src/test/java/com/csniico/notificationService/NotificationServiceApplicationTests.java
@@ -3,11 +3,6 @@ package com.csniico.notificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NotificationServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
